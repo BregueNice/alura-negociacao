4d5d8f42fb89ce42699d706c2d4579be8c7cfc6b
Vinicius Gabriel <viniciusrichard98@gmail.com>
2024-01-24 13:57:13 -0300

    finalizando as tipagens

--- app/app.ts
+++ app/app.ts
@@ -2,7 +2,7 @@ import { NegociacaoController } from "./controllers/negociacao-controller.js";
 import { NegociacoesView } from "./views/negociacoes-view.js";
 
 const controller = new NegociacaoController();
-const form = document.querySelector('.form');
+const form = document.querySelector('.form') as HTMLElement;
 form.addEventListener('submit', event =>{
     event.preventDefault();
     controller.adiciona();
