b1a52f9676375801c526999fe6830a344d6ec8a1
Vinicius Gabriel <viniciusrichard98@gmail.com>
2024-01-31 15:19:24 -0300

    criando decorator de escape

--- app/dist/js/views/view.js
+++ app/dist/js/views/view.js
@@ -4,23 +4,18 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 export class View {
-    constructor(seletor, escapar) {
-        this.escapar = false;
+    constructor(seletor) {
         this.elemento = document.querySelector(seletor);
-        if (escapar) {
-            this.escapar = escapar;
-        }
     }
     update(model) {
         let template = this.template(model);
-        if (this.escapar) {
-            template = template.replace(/<script>[\s\S]*?<script>/, '');
-        }
         this.elemento.innerHTML = template;
     }
 }
 __decorate([
-    logarTempoDeExecucao(true)
+    logarTempoDeExecucao(true),
+    inspect
 ], View.prototype, "update", null);
--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,24 +1,18 @@
+import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
-    private escapar = false;
-
-    constructor(seletor: string, escapar?: boolean) {
+    
+    constructor(seletor: string) {
         this.elemento = document.querySelector(seletor) as HTMLElement;
-        if (escapar) {
-            this.escapar = escapar;
-        }
     }
 
     @logarTempoDeExecucao(true)
+    @inspect
     public update(model: T): void {
         let template = this.template(model);
-
-        if (this.escapar) {
-            template = template.replace(/<script>[\s\S]*?<script>/, '');
-        }
         this.elemento.innerHTML = template;
     }
 
